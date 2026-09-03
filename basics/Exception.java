e73e98a8b9e55943005adf0117a0d5d9b9d94ac2
Yugal Kaushik <yugalkaushik14@gmail.com>
2026-09-04 00:00:11 +0530

    exception

---
+++ basics/Exception.java
@@ -0,0 +1,12 @@
+public class Exception {
+    public static void main(String[] args){
+        int[] arr = {1,2,3,4,5};
+
+        try{
+            System.out.println(arr[10]);
+        } catch(ArrayIndexOutOfBoundsException e){
+            System.out.println("Problem with code detected");
+        }
+        System.out.println("Program continues");
+    }
+}
