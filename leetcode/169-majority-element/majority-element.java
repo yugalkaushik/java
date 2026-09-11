413bfebabc46a1b1e32d4d5d41e023b06f6d6d68
Yugal Kaushik <yugalkaushik14@gmail.com>
2026-09-11 16:19:36 +0530

    Time: 16 ms (20.63%) | Memory: 52.8 MB (76.27%) - LeetSync

---
+++ leetcode/169-majority-element/majority-element.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        int n = nums.length;
+        Map<Integer,Integer> map = new HashMap<>();
+        for(int num:nums){
+            map.put(num, map.getOrDefault(num,0) + 1);
+            if(map.get(num)>n/2){
+                return num;
+            }
+        }
+        return -1;
+    }
+}
