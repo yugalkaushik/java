8f66b33f2842816acc7fc4c1b74cfd5978efca09
Yugal Kaushik <yugalkaushik14@gmail.com>
2026-09-15 22:03:42 +0530

    Time: 3 ms (56.33%) | Memory: 44.5 MB (47.05%) - LeetSync

---
+++ leetcode/125-valid-palindrome/valid-palindrome.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        int n = s.length();
+        s = s.toLowerCase();
+        int left = 0;
+        int right = n - 1;
+        while(left<right){
+            while(left<right && !Character.isLetterOrDigit(s.charAt(left))){
+                left++;
+            }
+            while(left<right && !Character.isLetterOrDigit(s.charAt(right))){
+                right--;
+            }
+            if(s.charAt(left)!=s.charAt(right)){
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
