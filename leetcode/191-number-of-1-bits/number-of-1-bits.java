e9ffde8e3f7d9a7e1c707f37dd2edb0f1292afbf
Yugal Kaushik <yugalkaushik14@gmail.com>
2026-09-16 13:39:23 +0530

    Time: 0 ms (100.00%) | Memory: 42.6 MB (11.71%) - LeetSync

---
+++ leetcode/191-number-of-1-bits/number-of-1-bits.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int hammingWeight(int n) {
+        int result = 0;
+        while(n!=0){
+            if((n & 1) == 1){
+                result++;
+            }
+            n = n>>1;
+        }
+        return result;
+    }
+}
