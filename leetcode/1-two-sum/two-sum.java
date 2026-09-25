28f3495aaad3dc0836654c79e322e2210a4f45ee
Yugal Kaushik <yugalkaushik14@gmail.com>
2026-09-25 20:09:29 +0530

    Time: 2 ms (99.40%) | Memory: 47.1 MB (41.16%) - LeetSync

---
+++ leetcode/1-two-sum/two-sum.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer,Integer> map = new HashMap<>();
+        int n = nums.length;
+        for(int i=0;i<n;i++){
+            int curr = target - nums[i];
+            if(map.containsKey(curr)){
+                return new int[]{map.get(curr),i};
+            }
+            map.put(nums[i],i);
+        }
+        return new int[]{};
+    }
+}
